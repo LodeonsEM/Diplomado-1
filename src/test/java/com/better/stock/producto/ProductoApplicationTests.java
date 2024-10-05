03c583e7ea2e1e6979e55c4dae2c1a0b508d0093
unknown <enrique.mares.tcp@gmail.com>
2024-10-05 00:26:34 -0600

    Primer commit

---
+++ src/test/java/com/better/stock/producto/ProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.better.stock.producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
